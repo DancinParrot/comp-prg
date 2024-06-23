72600c81e3072638ca709bcbdbcb162bd5868023
DancinParrot <kaizhetan@yahoo.com>
2024-06-23 13:04:56 +0800

    feat: add template for cp problems

---
+++ template.cpp
@@ -0,0 +1,13 @@
+// g++ -std=c++11 -O2 -Wall test.cpp -o build/test
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  // solution comes here
+}
