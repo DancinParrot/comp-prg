d418ef2b7eeb93dcaf4dd1e2cc14c7cdfadef1b9
DancinParrot <kaizhetan@yahoo.com>
2024-10-15 18:54:11 +0800

    Solved 227B Effective Approach - Hashmap lookup instead of linear search to resolve TLE

--- 227B.cpp
+++ 227B.cpp
@@ -1,76 +1,42 @@
 // g++ -std=c++11 -O2 -Wall test.cpp -o build/test
 #include <bits/stdc++.h>
-#include <cstdio>
+#include <unordered_map>
 using namespace std;
 
-int p_search(int b, vector<int> &nums) {
-  int c = 0;
-  for (auto it = nums.rbegin(); it != nums.rend(); ++it) {
-    c++;
-
-    if (*it == b) {
-      break;
-    }
-  }
-
-  return c;
-}
-
-int v_search(int b, vector<int> &nums) {
-  int c = 0;
-
-  for (auto &v : nums) {
-    c++;
-
-    if (v == b) {
-      break;
-    }
-  }
-
-  return c;
-}
+typedef long long ll;
 
 int main() {
   int n;
   cin >> n;
 
-  vector<int> nums;
+  // value, index of value stored in hashmap to allow O(1) lookup
+  unordered_map<int, int> nums;
 
+  int c = 1;
   int t = n;
   while (t--) {
     int tmp;
     cin >> tmp;
-    nums.push_back(tmp);
+    nums[tmp] = c;
+    c++;
   }
 
   int m;
   cin >> m;
-  vector<int> queries;
+  vector<int> q;
 
-  t = m;
-  while (t--) {
+  while (m--) {
     int tmp;
     cin >> tmp;
-    queries.push_back(tmp);
+    q.push_back(tmp);
   }
 
-  int v = 0;
-  int p = 0;
+  ll v = 0;
+  ll p = 0;
 
-  vector<int> v_ans(n + 1, 0);
-  vector<int> p_ans(n + 1, 0);
-
-  // n, m: 1e5 or 10^5, m can have repeats
-  // worst case: O(n * m), best case: O(n log m)
-  for (auto &b : queries) {
-    if (v_ans[b] == 0) {
-      // does not exist
-      v_ans[b] = v_search(b, nums);
-      p_ans[b] = p_search(b, nums);
-    }
-
-    v += v_ans[b];
-    p += p_ans[b];
+  for (auto b : q) {
+    v += nums[b];
+    p += abs(nums[b] - n) + 1;
   }
 
   cout << v << " " << p << endl;
