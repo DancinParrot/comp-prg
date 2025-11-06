a6b876a46e752490c276a7bdf50500a57b5c116b
dancincomrade <kaizhetan@yahoo.com>
2025-11-06 20:30:37 +0800

    refactor: remove unused stuff in template

--- template.cpp
+++ template.cpp
@@ -4,16 +4,12 @@ using namespace std;
 typedef long long ll;
 typedef vector<int> vi;
 
-void solve() { return; }
+void solve() {}
 
 int main() {
-  // ios::sync_with_stdio(0);
-  // cin.tie(0); don't mix C scanf() with C++ cin with this
-  // cout.tie(0);
-
-  // freopen("input.txt", "r", stdin);
-  // May use diff ./output.txt ./expected.txt to assert correctness
-  // freopen("output.txt", "w", stdout); // write stdout to output.txt
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
 
   solve();
   return 0;
