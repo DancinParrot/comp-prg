0a50c10cfa585cb6842177931d0075e5d8df42ff
dancincomrade <kaizhetan@yahoo.com>
2025-11-14 12:17:22 +0800

    feat: to revist 1985D, basic string processing and math

--- src/codeforces/1985D.cpp
+++ src/codeforces/1985D.cpp
@@ -28,12 +28,43 @@ const int INF = 1e9;
  */
 bool in(int n, int i) { return i >= 0 && i < n; }
 
-void solve() {}
+void solve() {
+  int n, m;
+  cin >> n >> m;
+  vector<vector<char>> g(n, vector<char>(m));
+
+  for (auto &r : g) {
+    for (auto &c : r) {
+      cin >> c;
+    }
+  }
+
+  debug(n, m, g);
+
+  // Center point (h, k) is directly below top point, and directly above bottom
+  // point
+  pi top = MP(INF, INF), bot = MP(-INF, -INF);
+  REP(i, 0, n) {
+    REP(j, 0, m) {
+      // get h by (top + bot) / 2
+      if (g[i][j] == '#') {
+        top = min(MP(i, j), top);
+        bot = max(MP(i, j), bot);
+      }
+    }
+  }
+
+  cout << ((top.F + bot.F) / 2) + 1 << ' ' << top.S + 1 << '\n';
+}
 
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  solve();
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
   return 0;
 }
