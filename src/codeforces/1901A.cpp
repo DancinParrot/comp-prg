8919aaabd449901c08384b5bd0db05005c6c8d62
DancinParrot <63692048+DancinParrot@users.noreply.github.com>
2026-01-17 14:53:41 +0800

    more problems

---
+++ src/codeforces/1901A.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#else
+#define debug(...)
+#define debugArr(...)
+#endif
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for (int i = a; i < b; i++)
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+const int INF = 1e9;
+
+/*
+ * This function checks to make sure index is within bounds of array.
+ * @param n Size of array.
+ * @param i Index to access.
+ * @return true if within bounds, else false
+ */
+bool in(int n, int i) { return i >= 0 && i < n; }
+
+void solve() {
+  int n, x;
+  cin >> n >> x;
+  vi nums(n);
+  for (auto &a : nums) {
+    cin >> a;
+  }
+
+  int max = nums[0];
+  int diff = 0;
+  REP(i, 1, n) {
+    diff = nums[i] - nums[i - 1];
+    if (diff > max)
+      max = diff;
+    debug(max, diff);
+  }
+
+  diff = (x - nums.back()) * 2;
+
+  if (diff > max)
+    max = diff;
+  cout << max << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
